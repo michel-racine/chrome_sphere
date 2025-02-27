17eb35c586c09561f4bb5caf3dabe03f5c4c6a88
michel-racine <91474769+michel-racine@users.noreply.github.com>
2025-02-27 11:45:49 -0500

    Update main.js
    Refactoring, cleaning, getting ready for collaboration

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let factor = 1;
-// Scene, Camera, Renderer
+// Scene, Lights, Camera, Renderer, Animation
 const scene = new THREE.Scene();
+
+
 const camera = new THREE.PerspectiveCamera(
   60,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.z = 3;
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2); // Strong directional light
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+directionalLight.position.copy(camera.position);
+
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -23,8 +32,8 @@ const skybox = loader.load([
 ]);
 scene.background = skybox;
 
+// Our cool mystical shape material and geometry
 const geometry = new THREE.IcosahedronGeometry(2.5, 0);
-
 const material = new THREE.MeshStandardMaterial({
   metalness: 1,
   roughness: 0,
@@ -32,36 +41,24 @@ const material = new THREE.MeshStandardMaterial({
   flatShading: true, // Ensures sharp edges on facets
 });
 
-// calling diamond a sphere here
-const sphere = new THREE.Mesh(geometry, material);
-sphere.rotation.y = Math.PI / 4; // Slight rotation for better look
-scene.add(sphere);
+const diamond = new THREE.Mesh(geometry, material);
+diamond.rotation.y = Math.PI / 4; // Slight rotation for better look
+scene.add(diamond);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2); // Strong directional light
-directionalLight.castShadow = true;
-scene.add(directionalLight);
 
-// Camera Position
-camera.position.z = 3;
-directionalLight.position.copy(camera.position);
-//directionalLight.position.set(0, 4, -4); // Light coming from top-right-front
-// Animation Loop
-let r = 0.25;
-let t = 0;
+let r = 0.25; // Radius
+let t = 0; // Theta or time delta
+
+diamond.position.z = -4;
+diamond.position.y = 0;
+diamond.position.z = -1;
+
 function animate() {
-  requestAnimationFrame(animate);
-  // sphere.rotation.y += 0.01; // Slow rotation
-
-  // sphere.position.x = Math.cos(t) * r;
-  // sphere.position.z = Math.sin(t) * r + 2.5;
-
   t += 0.01;
-  sphere.position.z = -4;
-  sphere.position.y = 0;
-  sphere.rotation.y += 0.0026 * factor;
-  // sphere.rotation.z += 0.0015 * factor;
-  // sphere.rotation.y = Math.cos(t);
-  sphere.rotation.z = Math.sin(t * 0.67);
+  diamond.rotation.y += 0.0026;
+  diamond.rotation.z = Math.sin(t * 0.67);
+
   renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
 animate();
